598ef4ba1f66fda1f44ddbc8a6b2dafb027d2979
Rodrigoslff <rodrigo.santos@opet.edu.br>
2021-05-06 21:45:54 -0300

    Schema

--- src/models/DepositoSchema.ts
+++ src/models/DepositoSchema.ts
@@ -5,11 +5,17 @@ const depositoSchema = new Schema(
         valor:{
             type: Number,
             required: [true,"É obrigatório!"],
+            min: [1, "Valor mínimo de R$ 1,00"],
         },
         conta: {
             type: Boolean,
             required:[true,"A conta alvo para o depósito é obrigatória!"],
         },
+        status:{
+            type: String,
+            enum: ["EFETUADO","AGENDADO","PENDENTE"],
+            uppercase: true
+          }
 
     },
     {
