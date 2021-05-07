e263f8445a1a4e18b2717f477ab666328fc2d8fc
rogerhlg <rogerhlg@outlook.com>
2021-05-06 23:10:56 -0300

    Ajuste nos exports

--- src/controllers/PixController.ts
+++ src/controllers/PixController.ts
@@ -1,36 +1,37 @@
-// import { Request, Response } from "express";
-// import { pixSchema } from "../models/PixSchema";
+import { Request, Response } from "express";
+import  PixSchema  from "../models/PixSchema";
 
-// class PixController{
-//     async listar(request: Request, response: Response) {
-//         const pixes = await pixSchema.find();
-//          response.status(200).json(pixes);
-//     }
+class PixController{
 
-//     async buscarPorId(request: Request, response: Response) {
-//         const { id } = request.params;
-//         // const ciclo = await CicloSchema.findById(id);
-//         // const ciclo = await CicloSchema.find({ _id : id});
-//         try {
-//             const pix = await pixSchema.findOne({ _id : id});
-//             if(pix === null) {
-//                 response.status(404).json({ msg: "Esse pix não existe!"});
-//             }
-//             response.status(200).json(pix);
-//         } catch (error) {
-//             response.status(400).json(error);
-//         }
+    async listar(request: Request, response: Response) {
+        const pixes = await PixSchema.create();
+         response.status(200).json(pixes);
+    }
+
+    async buscarPorId(request: Request, response: Response) {
+        const { id } = request.params;
+        // const ciclo = await CicloSchema.findById(id);
+        // const ciclo = await CicloSchema.find({ _id : id});
+        try {
+            const pix = await PixSchema.findOne({ _id : id});
+            if(pix === null) {
+                response.status(404).json({ msg: "Esse pix não existe!"});
+            }
+            response.status(200).json(pix);
+        } catch (error) {
+            response.status(400).json(error);
+        }
         
-//     }
+    }
 
-//     async cadastrar(request: Request, response: Response) {
-//         try {
-//             const novaPix = await pixSchema.create(request.body);
-//             response.status(201).json(novaPix);
-//         }catch (error) {
-//             response.status(400).json(error);
-//         }
-//     }
-// }
+    async cadastrar(request: Request, response: Response) {
+        try {
+            const novaPix = await PixSchema.create(request.body);
+            response.status(201).json(novaPix);
+        }catch (error) {
+            response.status(400).json(error);
+        }
+    }
+}
 
-// export { PixController }
+export { PixController }
--- src/models/PixSchema.ts
+++ src/models/PixSchema.ts
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
 const pixSchema = new Schema(
     {
@@ -17,4 +17,4 @@ const pixSchema = new Schema(
     }
 );
 
-export { pixSchema };
+export default model("pixes", pixSchema);
