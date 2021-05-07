42b9d925a3fc41b2b55c08a3078c474b5c3e6113
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-07 00:08:50 -0300

    Buscar por CPF

--- src/controllers/contaController.ts
+++ src/controllers/contaController.ts
@@ -2,18 +2,19 @@ import { Request, Response } from "express";
 import ContaSchema from "../models/ContaSchema";
 import DepositoSchema from "../models/DepositoSchema";
 import SaqueSchema from "../models/SaqueSchema";
+
 class ContaController{
     async listar(request: Request, response: Response) {
         const contas = await ContaSchema.find();
          response.status(200).json(contas);
     }
 
-    async buscarPorId(request: Request, response: Response) {
-        const { id } = request.params;
+    async buscarPorCpf(request: Request, response: Response) {
+        const { cpf } = request.params;
         // const ciclo = await CicloSchema.findById(id);
         // const ciclo = await CicloSchema.find({ _id : id});
         try {
-            const conta = await ContaSchema.findOne({ _id : id});
+            const conta = await ContaSchema.findOne({ CPF : cpf});
             if(conta === null) {
                 response.status(404).json({ msg: "A conta não existe!"});
             }
